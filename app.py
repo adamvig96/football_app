0208dd14413a82e8d667852811347071a71041e5
adamvig96 <vigadam@Vig-MacBook-Air.local>
2020-05-09 20:32:18 +0200

    final

--- app.py
+++ app.py
@@ -355,7 +355,7 @@ team_id_dic = {
 import dash
 import dash_html_components as html
 import dash_core_components as dcc
-from dash_html_components import Div, H3, H1, Link
+from dash_html_components import Div, H3, H1, Link, P
 import plotly.express as px
 
 external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
@@ -375,7 +375,23 @@ fig = px.scatter(
 )
 fig.update_layout(title_text="Closeness centrality and rating of players")
 
-#layout
+
+markdown_text = """
+This is our homework for the Applied Data-driven Algorithm Design course at Rajk College.
+We created a network of passes at a Chelsea-Arsenal Premier League Match. The game was held at the Chelsea home stadium [Stamford Bridge](https://hu.wikipedia.org/wiki/Stamford_Bridge_(stadion)) on January 21, 2020. The final score was 2:2.
+
+Each node represents a player who touched the ball during the game and the edges between them are the successfull passes.
+The edges are directed and weighted by the number of passes between the players.
+We created this project because networktools are hardly used for football analytics.
+
+For each player we calculated the closeness centrality, which is the normalized average length of the shortest path between the node and all other nodes in the graph.
+Therefore we could determine how important a player was for the team's passing game.
+ Football being a teamsport, we hoped that this value will strongly correlate with the actual rating of a player, which is coming from [WhoScored.com](https://www.whoscored.com/Matches/1376105/Preview/England-Premier-League-2019-2020-Chelsea-Arsenal).
+The left bottom plot shows the correlation between the closeness centrality and the ratings.
+"""
+
+
+# layout
 app.layout = html.Div(
     children=[
         H1(
@@ -388,12 +404,10 @@ app.layout = html.Div(
         ),
         html.Div(
             children=[
-                html.Div(
+                html.P(
                     children=[
-                        H3(children="mi ez a cucc?"),
-                        html.Element(
-                            children="babababababababababbabaaabababababababababab",
-                        ),
+                        H3(children="What is this all about?"),
+                        html.Element(dcc.Markdown(children=markdown_text),),
                     ],
                     className="six columns",
                 ),
